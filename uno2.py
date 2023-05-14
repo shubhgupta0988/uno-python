d8799de41ba0268d2024219e6c64ace3125803d0
Shubh Gupta <shubhgupta0988@gmail.com>
2023-05-14 20:03:53 +0530

    test

--- uno2.py
+++ uno2.py
@@ -1,6 +1,7 @@
 import random
 import time
 color = ('RED','GREEN','BLUE','YELLOW')
+
 rank = ('0','1','2','3','4','5','6','7','8','9','Skip','Reverse','Draw2','Draw4','Wild')
 ctype = {'0':'number','1':'number','2':'number','3':'number','4':'number','5':'number','6':'number',
             '7':'number','8':'number','9':'number','Skip':'action','Reverse':'action','Draw2':'action',
